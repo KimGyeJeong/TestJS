fbdd1d92d62c3927014ac85e1e1bf779e24e8662
KimGyeJeong <pmwkdo@gmail.com>
2022-11-01 03:01:50 +0900

    node.js Start

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -28,3 +28,31 @@ console.log(count2); //4
 
 
 console.log('hello world');
+
+var Human = function (type) {
+    this.type = type || 'human';
+};
+Human.isHuman = function (human) {
+    return human instanceof Human;
+};
+Human.prototype.breathe = function () {
+    alert('h-a-a-a-m');
+};
+
+var Zero = function (type, firstName, lastName) {
+    Human.apply(this, arguments);
+    this.firstName = firstName;
+    this.lastName = lastName;
+};
+Zero.prototype = Object.create(Human.prototype);
+Zero.prototype.constructor = Zero;
+Zero.prototype.sayName = function () {
+    alert(this.firstName + ' ' + this.lastName);
+}
+var oldZero = new Zero('human', 'Zero', 'Cho');
+Human.isHuman(oldZero); // true
+
+console.log(oldZero);   //Zero { type: 'human', firstName: 'Zero', lastName: 'Cho' }
+console.log(oldZero.constructor); // [Function: Zero]
+
+// console.log(Human.prototype.breathe()); //ERROR
