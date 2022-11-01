8486594ea5e26e4c326d0644e50b3c11ac3e0062
KimGyeJeong <pmwkdo@gmail.com>
2022-11-01 15:22:24 +0900

    study1101

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+    console.log('hello world');
+    // alert('oh! welcome');
+});
+
+// app.get('경로', function(요청(req), 응답(res)){
+    // 응답.send('반갑습니다');
+// })
+
+app.get('/helloTest', function(req, res){
+    res.send('Welcome');
+});
+
+app.get('welcomeTest', function(req, res){
+    res.send('Test. Welcome');
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+});
