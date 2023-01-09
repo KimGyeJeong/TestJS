fe49fae2c675a5573e9128e7925c4b224735ab15
KimGyeJeong <104893118+KimGyeJeong@users.noreply.github.com>
2023-01-09 09:15:54 +0900

    221216 자바스크립트 문제풀기

--- Vanilla/WitnNode/중앙값구하기.js
+++ Vanilla/WitnNode/중앙값구하기.js
@@ -4,9 +4,9 @@ let arr=[1,2,7,10,11];
 let arr2 = [1,-10,3,-23,5]
 
 arr2.sort();
-console.log(arr2)
+console.log(arr2);  //[ -10, -23, 1, 3, 5 ]
 arr2.sort((a,b)=>a-b);
-console.log(arr2)
+console.log(arr2)   //[ -23, -10, 1, 3, 5 ]
 
 let median_arrnum = arr[Math.floor(arr.length/2)];
 console.log(median_arrnum);
