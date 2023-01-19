40aaf01a057f52b084e73499d3cffd6ad9c5ec0a
KimGyeJeong <104893118+KimGyeJeong@users.noreply.github.com>
2023-01-19 11:46:41 +0900

    230119 공부

--- Vanilla/JS/study230119.js
+++ Vanilla/JS/study230119.js
@@ -115,3 +115,84 @@ console.log(bird_1 instanceof  Animal_1); // true
 console.log(bird_1.eat()); // eat
 console.log(bird_1.move()); // move
 console.log(bird_1.fly()); // fly
+
+
+console.log('--------------------------');
+
+//참고용 의사 클래스 상속패턴.. 사용하지 않음
+// 의사 클래스 상소갸(pseudo classical inheritance) 패턴
+var Animal = (function(){
+    function Animal(age, weight){
+        this.age = age;
+        this.weight = weight;
+    }
+    Animal.prototype.eat = function(){
+        return 'eat';
+    };
+    Animal.prototype.move  = function (){
+        return 'move';
+    };
+
+    return Animal;
+}());
+
+// Animal 생성자 함수를 상속하여 확장한 Bird 생성자 함수
+var Bird = (function(){
+    function Bird(){
+        // Animal 생성자 함수에게 this 와 인수를 전달하면서 호출
+        Animal.apply(this.arguments);
+    }
+
+    //Bird.prototype을 Animal.prototype을 프로토타입으로 갖는 객체로 교체
+    Bird.prototype = Object.create(Animal.prototype);
+    //Bird.prototype.constructor을 Animal 에서 Bird로 교체
+    Bird.prototype.constructor = Bird;
+
+    Bird.prototype.fly = function (){
+        return 'fly';
+    };
+
+    return Bird;
+}());
+
+var bird = new Bird(1,5);
+
+console.log(bird); // Bird { age: 1, weight: 5 }
+console.log(bird.eat()); // eat
+console.log(bird.move()); // move
+console.log(bird.fly()); // fly
+///////
+console.log('--------------------------');
+///////
+
+//extends 키워드
+// 상속을 통해 클래스를 확장하려면 extends 키워드를 사용하여 상속받을 클래스를 정의함.
+
+//수퍼(베이스, 부모)클래스
+class Base{
+    testNum = 2;
+    constructor(){
+        console.log('Base 생성자 호출');
+    }
+    getTestNum(){
+        return this.testNum;
+    }
+    setTestNum(num){
+        this.testNum = num;
+    }
+}
+
+//서브(파생, 자식)클래스
+class Derived extends Base{
+
+}
+
+const derived = new Derived();
+console.log(derived.getTestNum())
+derived.setTestNum(333);
+console.log(derived.getTestNum())
+
+
+// 동적 상속
+// extends 키워드는 클래스 ㅃㄴ만 아니라 생성자 함수를 상속받아 클래스를 확장할 수도 있다.
+// 단, extends 키워드 앞에는 반드시 클래슥 와야 한다.
