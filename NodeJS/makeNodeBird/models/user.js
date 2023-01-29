b9fbbcd5959e60c6d3517ff68695fbbc051d484a
KimGyeJeong <pmwkdo@gmail.com>
2023-01-29 12:29:39 +0900

    nodebird 0129

--- NodeJS/makeNodeBird/models/user.js
+++ NodeJS/makeNodeBird/models/user.js
@@ -1,16 +1,55 @@
-const oracledb = require('oracledb');
-const config = require('../models/dbconfig.js');
+const Sequelize = require('sequelize');
 
-oracledb.getConnection(config, (err, conn) =>{
-    if(err){
-        console.log(err);
-        return;
+class User extends Sequelize.Model {
+    static initiate(sequelize) {
+        User.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true,
+                unique: true,
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true,
+            },
+            provider: {
+                type: Sequelize.ENUM('local', 'kakao'),
+                allowNull: false,
+                defaultValue: 'local',
+            },
+            snsId: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: true,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
     }
-    conn.execute('SELECT * FROM testtable', (err, result) => {
-        if(err){
-            console.log(err);
-            return;
-        }
-        console.log(result.rows);
-    });
-})
+
+    static associate(db) {
+        db.User.hasMany(db.Post);
+        db.User.belongsToMany(db.User, {
+            foreignKey: 'followingId',
+            as: 'Followers',
+            through: 'Follow',
+        });
+        db.User.belongsToMany(db.User, {
+            foreignKey: 'followerId',
+            as: 'Followings',
+            through: 'Follow',
+        });
+    }
+};
+
+module.exports = User;
