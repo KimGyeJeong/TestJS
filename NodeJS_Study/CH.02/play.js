2a9395dba9dc075c9c9f5d47861bc10916eb3ffa
kimgyejeong <pmwkdo@gmail.com>
2023-06-03 17:28:17 +0900

    230603 공부02(화살표 함수)

--- NodeJS_Study/CH.02/play.js
+++ NodeJS_Study/CH.02/play.js
@@ -14,4 +14,22 @@ function summarizeUser(userName, userAge, userHasHobby) {
     );
 }
 
+
 console.log(summarizeUser(name, age, hasHobbies));    // Name is MAX, age is 29 and the user has hobbies: true
+
+const summarizeUser2 = (username, userage, userhasHobby) => {
+    return (
+        'Name is ' + username + ', age is ' + userage + ' and the user has hobbies: ' + userhasHobby
+    );
+}
+
+console.log(summarizeUser2(name, age, hasHobbies));   // Name is MAX, age is 30 and the user has hobbies: true
+
+const add = (a, b) => a + b;
+console.log(add(1, 2)); // 3
+
+const addOne = x => x+1;
+console.log(addOne(1)); // 2
+
+const addRandom = () => Math.random() + Math.random();
+console.log(addRandom());
