0f1c3d23019e58328504864761d84c282c42cfb4
kimgyejeong <pmwkdo@gmail.com>
2023-06-03 15:29:55 +0900

    230603 공부01

--- NodeJS_Study/CH.02/play.js
+++ NodeJS_Study/CH.02/play.js
@@ -5,3 +5,11 @@ var hasHobbies = true;
 console.log(name);  // MAX
 console.log(age);   // 29
 console.log(hasHobbies);    // true
+
+function summarizeUser(userName, userAge, userHasHobby) {
+    return (
+        'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
+    );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));    // Name is MAX, age is 29 and the user has hobbies: true
