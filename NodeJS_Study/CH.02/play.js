cdcf0486c2b88a9428b2876f89b8f2d49310cf1f
kimgyejeong <pmwkdo@gmail.com>
2023-06-05 13:31:42 +0900

    230605 공부02(비동기 코드와 프로미스2)

--- NodeJS_Study/CH.02/play.js
+++ NodeJS_Study/CH.02/play.js
@@ -1,12 +1,27 @@
-const fetchData =callback => {
-    setTimeout(() => {
-        callback('Done!');
-    }, 1500);
+const fetchData = callback => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+    return promise;
 };
 
 setTimeout(() => {
-console.log('Timer is done!');
-fetchData(text => console.log(text))
+    console.log('Timer is done!');
+// fetchData(text => console.log(text))
+    fetchData()
+        .then(done => {
+            console.log(done);
+
+            // fetchData().then(done2 => {
+            //     console.log(done2);
+            // });
+            return fetchData();
+        })
+        .then(done2 => {
+            console.log(done2);
+        });
 }, 2000);
 
 console.log('hello');
