b0f2d185dad844da9d07abae2f5f500c16bf5326
kimgyejeong <pmwkdo@gmail.com>
2023-06-05 13:23:56 +0900

    230604 공부02(비동기 코드와 프로미스1)

--- NodeJS_Study/CH.02/play.js
+++ NodeJS_Study/CH.02/play.js
@@ -1,80 +1,14 @@
-const person  = {
-    name : 'MAX',
-    age : 29,
-    greet : () =>
-    {
-        console.log('Hi, I am ' + this.name);
-    }
+const fetchData =callback => {
+    setTimeout(() => {
+        callback('Done!');
+    }, 1500);
 };
 
-console.log(person);
+setTimeout(() => {
+console.log('Timer is done!');
+fetchData(text => console.log(text))
+}, 2000);
 
-person.greet(); // Hi, I am undefined
+console.log('hello');
+console.log('hi');
 
-const person2 = {
-    name : 'MIN',
-    age : 30,
-    greet : function(){
-        console.log('Hi, I am ' + this.name);
-    }
-};
-person2.greet(); // Hi, I am MIN
-
-const person3 = {
-    name : 'MAX2',
-    greet(){
-        console.log('Hi, I am ' + this.name);
-    }
-};
-person3.greet(); // Hi, I am MAX2
-
-
-const hobbies = ['Sports', 'Cooking'];
-
-// for (let hobby of hobbies){
-//     console.log(hobby);
-// }
-
-console.log(hobbies.map(hobby =>
-     'Hobby : '+ hobby
-));
-console.log(hobbies)
-
-const copiedArray = hobbies.slice();
-console.log(`copiedArray : ${copiedArray}`);
-
-const copiedArray2 = [hobbies];
-console.log(`copiedArray2 : ${copiedArray2}`);
-
-console.log(hobbies == copiedArray2); // false
-console.log(hobbies === copiedArray2); // false
-
-const copiedArray3 = [...hobbies];
-console.log(`copiedArray3 : ${copiedArray3}`);
-console.log(hobbies == copiedArray3); // false
-console.log(hobbies === copiedArray3); // false
-
-const toArray = (arg1, arg2, arg3) => [arg1, arg2, arg3];
-console.log(toArray(1,2,3));
-
-const toArray2 = (...args) => args;
-console.log(toArray2(1,2,3));
-
-
-console.log(person);
-const printName = (person) => person.name;
-console.log(printName(person));
-
-const printName2 = ({name}) => name;
-console.log(printName2(person));
-
-const printName3 = ({name, age}) => `${name} : ${age}`;
-console.log(printName3(person));
-
-const {name, age} = person;
-console.log(name, age);
-
-
-console.log(hobbies);
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
