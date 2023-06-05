ce5c03fae39fb5e23609fac76bc153d71a59e2e2
kimgyejeong <pmwkdo@gmail.com>
2023-06-05 14:03:57 +0900

    230605 공부03(요청)

---
+++ NodeJS_Study/CH.03/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    console.log(req.url, req.method, req.headers);
+})
+
+server.listen(3000);
