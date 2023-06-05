0fbdba7abc6ec74ab8a0450a189c71c1d7e963bc
kimgyejeong <pmwkdo@gmail.com>
2023-06-05 15:30:51 +0900

    230605 공부03(라우터요청)

--- NodeJS_Study/CH.03/app.js
+++ NodeJS_Study/CH.03/app.js
@@ -4,6 +4,18 @@ const server = http.createServer((req, res) => {
     // console.log(req);
     console.log(req.url, req.method, req.headers);
 
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="message_form"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+
+        return res.end();
+    }
+
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
@@ -11,6 +23,7 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
 
     res.end();
+
 });
 
 server.listen(3000);
