b17d532b5e1454549c82f702f94e446e146a64fd
kimgyejeong <pmwkdo@gmail.com>
2023-06-05 16:20:16 +0900

    230605 공부03(요청 분문 분석)

--- NodeJS_Study/CH.03/app.js
+++ NodeJS_Study/CH.03/app.js
@@ -20,7 +20,21 @@ const server = http.createServer((req, res) => {
     }
 
     if(url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+
+        const body = [];
+        req.on('data', (chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(`parsedBody : ${parsedBody}`);
+            const message = parsedBody.split('=')[1];
+            console.log(`message : ${message}`);
+
+            fs.writeFileSync('message.txt', message);
+        });
+
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
