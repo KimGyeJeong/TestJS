be9d5d17692de25a14bcc35732a3c757d4215395
kimgyejeong <pmwkdo@gmail.com>
2023-06-13 23:11:56 +0900

    2306013 공부05(미들웨어 작동방식)

--- NodeJS_Study/CH.05/app.js
+++ NodeJS_Study/CH.05/app.js
@@ -7,13 +7,28 @@ const app = express();
 app.use((req, res, next) => {
     console.log('In the middleware!');
     next(); // next(); 가 있어야 아래의 미들웨어로 넘어감
+    // 응답을 보내지 않으면 next를 호출해야 함. 그러지 않으면 요청이 실패하고 다음 미들웨어로 이어지지 않음
+
+    // res.send('<h1>FIRST EXPRESS</h1>');
+    //여기서 응답을 하게 되면 아래의 미들웨어가 실행되지 않음. 물론 위의 next()는 없어야 함. 있으면 에러
 });
 
 app.use((req, res, next) => {
     console.log('In the another middleware!');
-    //...
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
 
 server.listen(3000);
+
+
+
+/*
+In the middleware!
+In the another middleware!
+In the middleware!
+In the another middleware!
+
+두번씩 찍히는 이유는 favicon.ico 요청 때문?
+ */
