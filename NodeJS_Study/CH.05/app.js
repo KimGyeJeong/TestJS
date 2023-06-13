02f5a823975b0cad7b19bc396748fb91f3648aa0
kimgyejeong <pmwkdo@gmail.com>
2023-06-13 23:19:10 +0900

    2306013 공부05(express.js 백그라운드 확인)

--- NodeJS_Study/CH.05/app.js
+++ NodeJS_Study/CH.05/app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -18,9 +16,7 @@ app.use((req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
 
 
 
