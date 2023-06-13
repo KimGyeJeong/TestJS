26429dc79348d99794c72b6b8cbb4a22c715b490
kimgyejeong <pmwkdo@gmail.com>
2023-06-13 23:01:44 +0900

    2306013 공부05(미들웨어 추가)

--- NodeJS_Study/CH.05/app.js
+++ NodeJS_Study/CH.05/app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // next(); 가 있어야 아래의 미들웨어로 넘어감
+});
+
+app.use((req, res, next) => {
+    console.log('In the another middleware!');
+    //...
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
