83f9ed46ae7e3a75743cebd10da7d35279ee5e4e
kimgyejeong <pmwkdo@gmail.com>
2023-06-14 22:39:08 +0900

    2306014 공부05(Express 라우터 사용)

--- NodeJS_Study/CH.05/app.js
+++ NodeJS_Study/CH.05/app.js
@@ -3,33 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
-//body-parser deprecated undefined extended: provide extended option app.js:6:20
-//옵션을 주어야함 extended : false . 비표준 대상의 분석이 가능한지 나타냄
 
+app.use(adminRoutes);
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
-});
-
-// app.use('/product', (req, res) => {    // 현재 get요청도 받음
-//     console.log(req.body);
-//     res.redirect('/');
-// });
-
-// app.get('/product', (req, res) => {    // get 요청 받기
-//     console.log(req.body);
-//     res.redirect('/');
-// });
-
-app.post('/product', (req, res) => {    // post 요청 받기
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(shopRoutes);
 
 app.listen(3000);
 
