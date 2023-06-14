f4967ffa21fe370a40b5546fade5294278a7cb49
kimgyejeong <pmwkdo@gmail.com>
2023-06-14 22:54:41 +0900

    2306014 공부05(경로 필터링)

--- NodeJS_Study/CH.05/routes/admin.js
+++ NodeJS_Study/CH.05/routes/admin.js
@@ -2,11 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+// router.get('/admin/add-product',(req, res, next) => {
+//     res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
+// });
+//
+// router.post('/admin/product', (req, res) => {    // post 요청 받기
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+
+//  /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
+    res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
 });
 
-router.post('/product', (req, res) => {    // post 요청 받기
+//  /admin/add-product => POST
+router.post('/add-product', (req, res) => {    // post 요청 받기
     console.log(req.body);
     res.redirect('/');
 });
