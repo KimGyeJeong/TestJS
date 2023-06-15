cc2856eabf677294c251e101b90b88365adb323a
kimgyejeong <pmwkdo@gmail.com>
2023-06-15 22:48:27 +0900

    2306015 공부05(HTML 페이지 서비스하기)

--- NodeJS_Study/CH.05/routes/admin.js
+++ NodeJS_Study/CH.05/routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const path = require('path');
+
 // router.get('/admin/add-product',(req, res, next) => {
 //     res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
 // });
@@ -13,8 +15,10 @@ const router = express.Router();
 
 //  /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
+    // res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button> </form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
+
 
 //  /admin/add-product => POST
 router.post('/add-product', (req, res) => {    // post 요청 받기
--- NodeJS_Study/CH.05/routes/shop.js
+++ NodeJS_Study/CH.05/routes/shop.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/',(req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    // res.send('<h1>Hello from Express!</h1>');
+    res.sendFile(path.join(__dirname,'../', 'views','shop.html'));
 });
 
-module.exports = router;
+module.exports = router; 
